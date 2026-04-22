843f207ee8b2063afe6e75bb64054c1a9fa9842f
georgiagriffin <thegeorgiagrace@gmail.com>
2026-04-22 19:40:00 -0400

    re interpretting uart

--- uart-hello-pi.py
+++ uart-hello-pi.py
@@ -1,7 +1,9 @@
 import serial
 
+# this print stuff that comes frm the usb uart converter that is plugged into USART6 on the PCB
+
 try:
-	ser = serial.Serial("/dev/ttyAMA0", 9600, timeout=1)
+	ser = serial.Serial("/dev/ttyUSB0", 9600, timeout=1)
 	print("Opened UART Successfully\n")
 except Exception as e:
 	print("ERROR: ", e);
