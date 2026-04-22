56c842a4b2714dd659433bf25546baeb2fc296e4
georgiagriffin <thegeorgiagrace@gmail.com>
2026-04-22 14:28:44 -0400

    chaged baud rate - yashvi pushing

--- uart-hello-pi.py
+++ uart-hello-pi.py
@@ -1,7 +1,7 @@
 import serial
 
 try:
-	ser = serial.Serial("/dev/ttyAMA0", 115200, timeout=1)
+	ser = serial.Serial("/dev/ttyAMA0", 9600, timeout=1)
 	print("Opened UART Successfully\n")
 except Exception as e:
 	print("ERROR: ", e);
